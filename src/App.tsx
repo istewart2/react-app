cf6768495e20805c02a2d4d538936cf4607fb4a7
Iain Stewart <iainstewart08@gmail.com>
2023-05-09 09:21:28 +0100

    Use button to display dismissable alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import Alert from './components/Alert';
 import Button from './components/Button';
@@ -5,18 +6,28 @@ import ListGroup from './components/ListGroup';
 
 const App = () => {
   const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
+  const [showAlert, setShowAlert] = useState(false);
+
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
+
   const handleButtonClick = () => {
-    console.log('Button clicked');
+    setShowAlert(true);
+  };
+
+  const handleAlertDismiss = () => {
+    setShowAlert(false);
   };
 
   return (
     <div>
-      <Alert>
-        <h1>Hello World</h1>
-      </Alert>
+      {showAlert && (
+        <Alert onClose={handleAlertDismiss}>
+          <h1>Hello World</h1>
+        </Alert>
+      )}
+
       <Button onButtonClick={handleButtonClick}>This is a button</Button>
       <ListGroup
         items={items}
