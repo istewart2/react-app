16c5460362741205e5075a1587a563bc338d6cba
Iain Stewart <iainstewart08@gmail.com>
2023-05-11 23:27:22 +0100

    Add nested object / array update sample

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,44 @@ import { BsFillCalendarFill } from 'react-icons/bs';
 const App = () => {
   const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
   const [showAlert, setShowAlert] = useState(false);
+  const [liked, setLiked] = useState(false);
+
+  // updating nested object example
+  const [customer, setCustomer] = useState({
+    name: 'John',
+    address: {
+      city: 'San Francisco',
+      zipCode: 94111,
+    },
+  });
+
+  const handleUpdateCustomer = () => {
+    console.log(customer);
+    setCustomer({
+      ...customer,
+      address: {
+        ...customer.address,
+        zipCode: 94112,
+      },
+    });
+    console.log(customer);
+  };
+
+  // update array
+  const [tags, setTags] = useState(['happy', 'cheerful']);
+
+  const handleUpdateArray = () => {
+    console.log(tags);
+    // add
+    setTags([...tags, 'exciting']);
+
+    // remove
+    setTags(tags.filter((tag) => tag !== 'happy'));
+
+    // update
+    setTags(tags.map((tag) => (tag === 'happy' ? 'happiness' : tag)));
+    console.log(tags);
+  };
 
   const handleSelectItem = (item: string) => {
     console.log(item);
@@ -28,9 +66,18 @@ const App = () => {
           <h1>Hello World</h1>
         </Alert>
       )}
-      <BsFillCalendarFill size='40' />
+      <BsFillCalendarFill
+        size='40'
+        onClick={() => {
+          console.log('clicked');
+          setLiked((current) => !current);
+        }}
+        color={liked ? 'red' : ''}
+      />
       <br />
       <Button onButtonClick={handleButtonClick}>This is a button</Button>
+      <Button onButtonClick={handleUpdateCustomer}>Update customer</Button>
+      <Button onButtonClick={handleUpdateArray}>Update array</Button>
       <ListGroup
         items={items}
         heading='Cities'
